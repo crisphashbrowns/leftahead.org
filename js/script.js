f5f7b8753d6a09f8853cceca21b66a2e4668b0d9
Levi <levi@technomancy.dev>
2023-09-14 00:51:01 +0200

    You did not have the JS to select and configure the slider on mobile.

--- js/script.js
+++ js/script.js
@@ -288,6 +288,68 @@ $('.home-banner').mousemove(function(e) {
                 disableFocusListener: false,
             }
         });
+
+
+
+        $("#rev_slider_mobile_1_5").show().revolution({
+            sliderType: "standard",
+            jsFileLocation: "//localhost/reveditor/revslider/public/assets/js/",
+            sliderLayout: "fullscreen",
+            dottedOverlay: "none",
+            delay: 3000,
+            navigation: {
+                keyboardNavigation: "off",
+                keyboard_direction: "horizontal",
+                mouseScrollNavigation: "off",
+                mouseScrollReverse: "default",
+                onHoverStop: "off",
+                touch: {
+                    touchenabled: "on",
+                    touchOnDesktop: "on",
+                    swipe_threshold: 75,
+                    swipe_min_touches: 1,
+                    swipe_direction: "horizontal",
+                    drag_block_vertical: false
+                },
+
+            },
+            responsiveLevels: [1240, 1024, 778, 480],
+            visibilityLevels: [1240, 1024, 778, 480],
+            gridwidth: [1140, 1024, 778, 480],
+            gridheight: [700, 768, 960, 420],
+            lazyType: "none",
+            parallax: {
+                type: "mouse",
+                origo: "enterpoint",
+                speed: 400,
+                speedbg: 0,
+                speedls: 0,
+                levels: [2, 3, 5, 10, 25, 30, 35, 40, 45, 46, 47, 48, 49, 50, 51, 55],
+                disable_onmobile: "on"
+            },
+            shadow: 0,
+            spinner: "off",
+            stopLoop: "off",
+            stopAfterLoops: -1,
+            stopAtSlide: -1,
+            shuffle: "off",
+            autoHeight: "off",
+            fullScreenAutoWidth: "off",
+            fullScreenAlignForce: "off",
+            fullScreenOffsetContainer: "",
+            fullScreenOffset: "",
+            disableProgressBar: "on",
+            hideThumbsOnMobile: "off",
+            hideSliderAtLimit: 0,
+            hideCaptionAtLimit: 0,
+            hideAllCaptionAtLilmit: 0,
+            debugMode: false,
+            fallbacks: {
+                simplifyAll: "off",
+                nextSlideOnWindowFocus: "off",
+                disableFocusListener: false,
+            }
+        });
     }
 
 
